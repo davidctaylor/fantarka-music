55f0514b3b27b25288d311a87d5dec3cd3f744b9
David Taylor <david.taylor@aol.com>
2021-01-07 18:06:36 -0700

    github pages build

---
+++ docs/precache-manifest.7619497935678eca5f4213e73221374b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87b4c7695564b09f0e8c5f59222c3bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba360a2274dbe9ed8856",
+    "url": "/static/css/2.b758e220.chunk.css"
+  },
+  {
+    "revision": "1ce60d5870c156f366c6",
+    "url": "/static/css/main.b28c2d2f.chunk.css"
+  },
+  {
+    "revision": "ba360a2274dbe9ed8856",
+    "url": "/static/js/2.e8d1c727.chunk.js"
+  },
+  {
+    "revision": "3644c4282cc8457b89fc5d80f7163c08",
+    "url": "/static/js/2.e8d1c727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ce60d5870c156f366c6",
+    "url": "/static/js/main.143d9409.chunk.js"
+  },
+  {
+    "revision": "5859951aa0e87e75b222",
+    "url": "/static/js/runtime-main.b4cd7978.js"
+  }
+]);
